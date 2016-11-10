03980b63c7f7fa5bfd6a74e23613bcf82392105a
Adam <adam@adambard.com>
2016-11-09 22:10:35 -0800

    Fix encoding test, then make tests actually fail.

--- Rakefile
+++ Rakefile
@@ -1,11 +1,19 @@
 task default: %w[test]
 
 task :test do
+  failed = false
   Dir["./tests/*.rb"].each do |test_file|
     begin
-    	ruby test_file
+      ruby test_file
+      puts ""
     rescue
       puts "FAILED #{test_file}!"
+      puts ""
+      failed = true
     end
+  end
+
+  if failed
+    exit 0
   end
 end
--- tests/encoding.rb
+++ tests/encoding.rb
@@ -9,16 +9,21 @@ markdown_files.each do |file|
     case detection[:encoding]
     when 'UTF-8'
       $file_count = $file_count + 1
-    when 'ISO-8859-1'
-      $file_count = $file_count + 1
     else
-      puts "#{file} was detected as #{detection[:encoding]} encoding! Please save the file in UTF-8!"
+      enc = detection[:encoding]
+      if not enc.start_with? 'ISO-8859-'
+        puts "#{file} was detected as #{detection[:encoding]} encoding! Please save the file in UTF-8!"
+      else
+        $file_count += 1
+      end
     end
   rescue Exception => msg
     puts msg
   end
 end
+
 files_failed = markdown_files.length - $file_count
+
 if files_failed != 0
   puts "FAILURE!!! #{files_failed} files were unable to be validated as UTF-8!"
   puts "Please resave the file as UTF-8."
